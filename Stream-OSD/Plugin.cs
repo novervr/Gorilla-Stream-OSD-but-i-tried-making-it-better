dacf9c8031ffc1c31dfde9df4e37c96c725401ed
Administrator <Administrator@GRAZE>
2022-11-17 16:42:10 +0000

    Add project files.

---
+++ Stream-OSD/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using Photon.Pun;
+using System;
+using UnityEngine;
+using UnityEngine.XR;
+using Utilla;
+using GorillaLocomotion;
+using GorillaNetworking;
+
+namespace Stream_OSD
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+   
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public GUIStyle guiStyle = new GUIStyle();
+        private readonly XRNode lNode = XRNode.LeftHand;
+        bool sec;
+        bool show;
+        private void OnGUI()
+        {
+            guiStyle.fontSize = 20;
+            guiStyle.normal.textColor = Color.white;
+            bool inRoom = PhotonNetwork.InRoom;
+            GUI.Label(new Rect(15f, 25f, 120f, 60f), "Graze", guiStyle);
+            if (inRoom)
+            {
+                GUI.Label(new Rect(900f, 10f, 200f, 20f), "Current Code:" + PhotonNetwork.CurrentRoom.Name, guiStyle);
+               
+               GUI.Label(new Rect(900f, 35f, 200f, 40f), "There are " + PhotonNetwork.CurrentRoom.PlayerCount.ToString() + " People", guiStyle);
+                GUI.Label(new Rect(1800f, 10f, 200f, 40f), "GameMode:", guiStyle);
+                GUI.Label(new Rect(1800f, 35f, 200f, 40f), GorillaGameManager.instance.GameMode(), guiStyle);
+            }
+            if (!inRoom)
+            {
+                GUI.Label(new Rect(900f, 10f, 200f, 20f), "Not In a Code", guiStyle);
+                GUI.Label(new Rect(1800f, 10f, 200f, 40f), "GameMode:", guiStyle);
+            }
+        }
+    }
+
+}
---
+++ Stream-OSD/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Stream_OSD
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.stream-osd";
+        public const string Name = "Stream_OSD";
+        public const string Version = "1.0.0";
+    }
+}
